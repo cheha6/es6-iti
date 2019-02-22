673e4971c7137b9032edb83e85a88d386197d1dc
Shehab ElHariry <cheha6@gmail.com>
2019-02-22 21:29:25 +0200

    added lecture 1 material

---
+++ lec-1/scripts.js
@@ -0,0 +1,44 @@
+const webDesigner = {
+  name: 'YOUR NAME',
+  yearsAlive: [1992,2018],
+  imgSrc: 'user.jpg',
+  skills: ['Photoshop', 'HTML', 'CSS', 'JS'],
+  diet: {
+    food: 'Indomi',
+    drinks: ['Coffee', 'Nescafe', 'tea', 'water']
+  }
+}
+
+//write your name instead of the YOUR NAME placeholder
+
+
+/*write a getAge function that takes the years alive array and returns your age,
+ save the value you return in a const of name age*/
+
+
+/*Divide the Webdesign skills into designSkills and developmentskills using ES6*/
+
+
+/*add Those extra skills to the developmentskills array ['ES6', 'ES2015'] and add all of them into a new updatedDevSkills const*/
+
+
+/*destructure the diet array and using new ES6 write a function that returns drinks that contain the letter t*/
+
+
+
+// function buildID () {
+//   const userID = `
+//   <div class="inner-container">
+//     <img src="img" alt="">
+//     <div class="info">
+//       <h1>placeholder</h1>
+//       <p><span class="text-grey">Age:</span> placeholder</p>
+//       <p><span class="text-grey">Design Skills:</span> placeholder</p>
+//       <p><span class="text-grey">Dev Skills:</span> placeholder</p>
+//       <p><span class="text-grey">Food:</span> placeholder</p>
+//       <p><span class="text-grey">Drinks:</span>placeholder</p>
+//     </div>
+//   </div>
+//   `;
+//   return userID
+// }
